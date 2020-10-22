61b8bab1d32550d9d6869d5eff8c949b45c35461
benbouillet <benjamin.bouillet@decathlon.com>
2020-10-22 15:47:46 +0200

    Cleaned repo

--- env_setup.sh
+++
@@ -1,8 +0,0 @@
-export https_proxy=http://proxy-internet-aws-eu.subsidia.org:3128
-export http_proxy=http://proxy-internet-aws-eu.subsidia.org:3128
-export no_proxy=169.254.169.254,127.0.0.1
-git config --global credential.helper store
-git config --global user.email "antoine.schwartz@decathlon.com"
-git config --global user.name "Antoine Schwartz"
-conda env create environment.yml
-source activate forecast-modeling-demand
