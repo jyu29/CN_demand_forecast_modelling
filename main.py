2a2acca3e4b9c04c6fa33745e19e216c161e484a
benbouillet <benjamin.bouillet@decathlon.com>
2020-10-26 15:16:33 +0100

    Test implementation of main.py

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ Python script to orchestrate demand forecast modeling:
 - Pops instance to  preprocess train a DeepAR model, then output predictions
 @author: benbouillet ( Benjamin Bouillet )
 """
+import s3fs
 import src.utils as ut
+import src.sagemaker_utils as su
 import argparse
 
 if __name__ == '__main__':
@@ -15,17 +17,22 @@ if __name__ == '__main__':
                         help="'dev' or 'prod', to set the right configurations")
     parser.add_argument('--cutoff', default=ut.get_current_week(), help="cutoff in format YYYYWW or 'today'")
     args = parser.parse_args()
+
+    # Defining variables
     environment = args.environment
     cutoff = args.cutoff
+    run_name = f"{environment}-{cutoff}"
 
     # import parameters
-    params_full_path = f"s3://fcst-config/forecast-modeling-demand/{environment}.json"
-    params = ut.read_json(params_full_path)
+    params_full_path = f"s3://fcst-config/forecast-modeling-demand/{environment}.yml"
+    params = ut.read_yml(params_full_path)
 
     print(f"Starting modeling for cutoff {cutoff} in {environment} environment with parameters:")
-    ut.pretty_print_json(params)
+    ut.pretty_print_dict(params)
 
     # Define df_jobs
+    df_jobs = su.generate_df_jobs(run_name, [cutoff], params['buckets']['refined-data'], params['paths']['run_input_path'])
+    df_jobs.apply(lambda row: su.generate_input_data(row, s3fs.S3FileSystem(), params), axis=1)
     # df_jobs = some_function_to_generate_df_jobs()
 
     # Export parameter on AWS S3
