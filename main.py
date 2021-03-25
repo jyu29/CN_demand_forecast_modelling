6514943ec01d2a5b4dcb15293f17dd1f9885bb6b
Antoine-Schwartz <4f28ae1ee829c99a2846eca0a616d72e2da2690d>
2021-03-25 15:34:12 +0000

    feat: update yml files & store_openings csv

--- main.py
+++ main.py
@@ -1,60 +1,92 @@
-"""
-Python script to orchestrate demand forecast modeling:
-- Engineers features & prepares data in DeepAR format
-- Creates Training Docker Image
-- Pops instance to  preprocess train a DeepAR model, then output predictions
-@author: benbouillet ( Benjamin Bouillet )
-"""
 import argparse
-import json
 
-import src.sagemaker_utils as su
+import pandas as pd
+
 import src.utils as ut
+import src.data_handler as dh
+import src.sagemaker_utils as su
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--logging_lvl', choices=['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG'],
+                    default="INFO", help="Level for logger")
+args = parser.parse_args()
+su.logger.setLevel(args.logging_lvl)
+dh.logger.setLevel(args.logging_lvl)
 
-if __name__ == '__main__':
-    
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--environment', choices=['dev', 'prod', 'dev_old'], default="dev",
-                        help="'dev' or 'prod', to set the right configurations")
-    parser.add_argument('--list_cutoff', default=str('today'), help="List of cutoffs in format YYYYWW between brackets or 'today'")
-    parser.add_argument('--run_name', help="Run Name for file hierarchy purpose")
-    args = parser.parse_args()
+list_cutoff = [202105]
+run_name = 'testrun'
+environment = 'test'
 
-    # Defining variables
-    environment = args.environment
-    if args.list_cutoff == 'today':
-        list_cutoff = [ut.get_current_week()]
-    else:
-        list_cutoff = json.loads(args.list_cutoff)
+# Data loading
+df_model_week_sales = pd.read_parquet('~/Downloads/modeling_data/model_week_sales/')
+df_model_week_tree = pd.read_parquet('~/Downloads/modeling_data/model_week_tree/')
+df_model_week_mrp = pd.read_parquet('~/Downloads/modeling_data/model_week_mrp/')
+df_store_openings = pd.read_csv('~/Downloads/modeling_data/store_openings/store_openings.csv', sep=";")
+df_holidays = pd.read_csv('~/Downloads/modeling_data/holidays/holidays.csv', sep=";")
+df_test_feat = pd.read_csv('~/Downloads/modeling_data/test_dynamic_specific_feat/test_feat.csv', sep=";")
 
+# Generate empty df_jobs
+refined_data_specific_path = ut.to_uri(ut.import_raw_config(environment)['buckets']['refined_data_specific'],
+                                       ut.import_raw_config(environment)['paths']['refined_specific_path']
+                                       )
+algorithm = ut.import_raw_config(environment)['modeling_parameters']['algorithm']
+df_jobs = su.generate_df_jobs(list_cutoff=list_cutoff,
+                              run_name=run_name,
+                              algorithm=algorithm,
+                              refined_data_specific_path=refined_data_specific_path
+                              )
+
+if __name__ == "__main__":
     for cutoff in list_cutoff:
-        assert type(cutoff) == int
-    
-    assert type(args.run_name) == str
-    run_name = args.run_name
+        # Data/Features init
+        base_data = {'model_week_sales': df_model_week_sales,
+                     'model_week_tree': df_model_week_tree,
+                     'model_week_mrp': df_model_week_mrp
+                     }
+        
+        df_static_tree = df_model_week_tree[df_model_week_tree['week_id'] == cutoff].copy()
+        static_features = {'model_identifier': pd.DataFrame({'model_id': df_static_tree['model_id'],
+                                                             'model_identifier': df_static_tree['model_id']}),
+                           'family_id': df_static_tree[['model_id', 'family_id']],
+                           'sub_department_id': df_static_tree[['model_id', 'sub_department_id']],
+                           'department_id': df_static_tree[['model_id', 'department_id']],
+                           'product_nature_id': df_static_tree[['model_id', 'product_nature_id']],
+                           'univers_id': df_static_tree[['model_id', 'univers_id']],
+                           }
+        
+        global_dynamic_features = {'store_openings': {'dataset': df_store_openings,
+                                                      'projection': 'ffill'},
+                                   'holidays': {'dataset': df_holidays,
+                                                'projection': 'as_provided'}
+                                   }
 
-    # import parameters
-    params_full_path = f"config/{environment}.yml"
-    params = ut.read_yml(params_full_path)
+        specific_dynamic_features = None
+        
+        train_path = df_jobs[df_jobs['cutoff'] == cutoff].loc[:, 'train_path'].values[0]
+        predict_path = df_jobs[df_jobs['cutoff'] == cutoff].loc[:, 'predict_path'].values[0]
 
-    # Getting variables for code readability below
-    algo = params['functional_parameters']['algorithm']
+        refining_params = ut.import_refining_config(environment=environment,
+                                                    cutoff=cutoff,
+                                                    run_name=run_name,
+                                                    train_path=train_path,
+                                                    predict_path=predict_path
+                                                    )
 
-    # Building custom full paths
-    refined_specific_path = params['paths']['refined_specific_path']
-    params['paths']['refined_specific_path_full'] = f"{refined_specific_path}{run_name}/{algo}/"
+        refining_handler = dh.data_handler(base_data=base_data,
+                                           static_features=static_features,
+                                           global_dynamic_features=global_dynamic_features,
+                                           specific_dynamic_features=specific_dynamic_features,
+                                           **refining_params
+                                           )
 
-    print(f"Starting modeling run '{run_name}' for cutoff {list_cutoff} in {environment} environment with parameters:")
-    ut.pretty_print_dict(params)
+        refining_handler.execute_data_refining_specific()
 
-    # SAGEMAKER #
-    sm_handler = su.SagemakerHandler(run_name, list_cutoff, params)
-    # Generating df_jobs
-    sm_handler.generate_df_jobs()
-    # Feature generation
-    sm_handler.generate_input_data_all_cutoffs()
-    # Training Job
-    sm_handler.launch_training_jobs()
-    # Transform job
-    sm_handler.launch_transform_jobs()
+    sagemaker_params = ut.import_sagemaker_params(environment=environment)
+
+    # modeling_handler = su.SagemakerHandler(run_name=run_name,
+    #                                        df_jobs=df_jobs,
+    #                                        **sagemaker_params)
+
+    # modeling_handler.launch_training_jobs()
+
+    # modeling_handler.launch_transform_jobs()
