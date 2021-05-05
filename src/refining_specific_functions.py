a0e055acb724539c6ccd25e9f46c9218aaabab04
Antoine-Schwartz <antoine.schwartz@decathlon.com>
2021-05-05 16:47:07 +0000

    delete unused specific functions & rework some + update docstrings

--- src/refining_specific_functions.py
+++ src/refining_specific_functions.py
@@ -57,58 +57,27 @@ def pad_to_cutoff(df_ts: pd.DataFrame,
     return df
 
 
-def zero_padding_rec(df, df_model_week_sales, rec_length):
-
-    # Create a complete TS dataframe
-    all_model = df['model_id'].sort_values().unique()
-    all_week = df_model_week_sales \
-        .loc[df_model_week_sales['week_id'] <= df['week_id'].max(), 'week_id'] \
-        .sort_values() \
-        .unique()
-
-    w, m = pd.core.reshape.util.cartesian_product([all_week, all_model])
-
-    complete_ts = pd.DataFrame({'model_id': m, 'week_id': w})
-
-    # Add dates
-    complete_ts['date'] = week_id_to_date(complete_ts['week_id'])
-
-    # Add current sales from df
-    complete_ts = pd.merge(complete_ts, df, how='left')
-
-    # Calculate real age & total length of each TS
-    ts_start_end_date = complete_ts \
-        .loc[complete_ts['sales_quantity'].notnull()] \
-        .groupby(['model_id']) \
-        .agg(start_date=('date', 'min'),
-             end_date=('date', 'max')) \
-        .reset_index()
-
-    complete_ts = pd.merge(complete_ts, ts_start_end_date, how='left')
-
-    complete_ts['age'] = ((pd.to_datetime(complete_ts['date']) - pd.to_datetime(complete_ts['start_date'])
-                           ) / np.timedelta64(1, 'W')).astype(int) + 1
-
-    complete_ts['length'] = ((pd.to_datetime(complete_ts['end_date']) - pd.to_datetime(complete_ts['date'])
-                              ) / np.timedelta64(1, 'W')).astype(int) + 1
-
-    # Pad NaN quantities from 'rec_length' weeks ago
-    complete_ts.loc[((complete_ts['length'] <= rec_length) & (complete_ts['age'] <= 0)), 'sales_quantity'] = 0
-
-    # Format
-    complete_ts = complete_ts[list(df)].dropna(subset=['sales_quantity']).reset_index(drop=True)
-    complete_ts['sales_quantity'] = complete_ts['sales_quantity'].astype(int)
-
-    return complete_ts
-
-
 def cold_start_rec(df,
                    df_model_week_sales,
                    df_model_week_tree,
                    rec_length,
                    rec_cold_start_group=['family_label']
                    ):
+    """
+    Create a fake sales history for models that started too recently.
 
+    Args:
+        df (pd.DataFrame): The sales of the time series pool on which we want to apply a cold-start 
+            reconstruction
+        df_model_week_sales (pd.DataFrame): Sales of all available time series
+        df_model_week_tree (pd.DataFrame): Tree structure information of all available time series
+        rec_length (int): The minimum expected size of each time series after reconstruction 
+        rec_cold_start_group (list): The list of aggregation keys (of the tree structure) on which 
+            to calculate the reconstruction properties
+
+    Returns:
+        complete_ts (pd.DataFrame): The sales of the time series pool including the reconstruction
+    """
     # Create a complete TS dataframe
     all_model = df['model_id'].sort_values().unique()
     all_week = df_model_week_sales \
@@ -201,59 +170,62 @@ def cold_start_rec(df,
     return complete_ts
 
 
-def check_weeks_df(df, min_week, cutoff, future_weeks=0, week_column='week_id'):
+def initialize_df_dynamic_features(df, cutoff, prediction_length):
     """
-    Checks the presence of all past & future weeks in a global feature pandas DataFrame
+    Initialize the dynamic features dataframe on which all dynamic features will be joined.
+    
+    Dynamic features must be available in the future, therefore the expected size of the dataframe 
+    must correspond to the observed sales for each model plus its projection over the forecast horizon.
 
     Args:
-        df (pandas.DataFrame): Pandas DataFrame to check
-        min_week (int): Minimum week to check in the DataFrame (YYYYWW ISO Format)
-        cutoff (int): first week of forecast (YYYYWW ISO Format)
-        future_weeks (int): Expected number of weeks in the future
-        week_column (str): Name of the column on which to make the checks
+        df (pd.DataFrame): The sales dataframe
+        cutoff (int): The week_id of the cutoff, which corresponds to the first forecast step 
+        prediction_length (int): The forecast horizon
+
+    Returns:
+        df_dynamic_features (pd.DataFrame): The initialized dynamic features dataframe
     """
-    feature_name = ', '.join((set(df.columns) - set(['model_id', 'week_id'])))
-    expected_week_range = generate_expected_week_range(min_week, cutoff, future_weeks)
-    actual_week_range = df[week_column].unique()
+    model_ids = df['model_id'].unique()
+    
+    future_date_range = pd.date_range(start=week_id_to_date(cutoff), periods=prediction_length, freq='W')
+    future_date_range_weeks = [date_to_week_id(w) for w in future_date_range]
 
-    for w in expected_week_range:
-        assert w in actual_week_range, f"Week {w} is not in {feature_name} dataframe."
+    w, m = pd.core.reshape.util.cartesian_product([future_date_range_weeks, model_ids])
 
+    future_model_week = pd.DataFrame({'model_id': m, 'week_id': w})
 
-def generate_expected_week_range(min_week, cutoff, future_weeks):
-    min_date = week_id_to_date(min_week)
-    cutoff_date = week_id_to_date(cutoff)
-    max_date = cutoff_date + dt.timedelta(weeks=future_weeks - 1)
-
-    date_range = pd.date_range(start=min_date, end=max_date, freq='W')
-    expected_week_range = [date_to_week_id(d) for d in date_range]
-
-    return expected_week_range
+    df_dynamic_features = df[['model_id', 'week_id']].append(future_model_week)
+    df_dynamic_features.sort_values(['model_id', 'week_id'], inplace=True)
+        
+    return df_dynamic_features
 
 
-def generate_empty_dyn_feat_global(df_sales, min_week, cutoff, future_projection):
-    expected_week_range = generate_expected_week_range(min_week, cutoff, future_projection)
-    expected_models = df_sales['model_id'].unique()
+def is_rec_feature_processing(df, cutoff, prediction_length):
+    """
+    Process the dynamic features `is_rec` based on `cold_start_rec` output.
+    
+    Dynamic features must be available in the future, therefore the feature 
+    is projected on the horizon with a value of 0.
+    
+    Args:
+        df (pd.DataFrame): The sales dataframe which includes the reconstructed weeks information
+        cutoff (int): The week_id of the cutoff, which corresponds to the first forecast step 
+        prediction_length (int): The forecast horizon
 
-    m, w = pd.core.reshape.util.cartesian_product([expected_models, expected_week_range])
+    Returns:
+        df_is_rec (pd.DataFrame): The 'is_rec' dynamic feature dataframe
+    """
+    model_ids = df['model_id'].unique()
+    
+    future_date_range = pd.date_range(start=week_id_to_date(cutoff), periods=prediction_length, freq='W')
+    future_date_range_weeks = [date_to_week_id(w) for w in future_date_range]
 
-    df_empty_dyn_feat_global = pd.DataFrame({'week_id': w,
-                                             'model_id': m})
+    w, m = pd.core.reshape.util.cartesian_product([future_date_range_weeks, model_ids])
 
-    return df_empty_dyn_feat_global
-
-
-def is_rec_feature_processing(df_sales, cutoff, prediction_length):
-    # Adding is_rec dynamic feat
-    df_is_rec = df_sales[['model_id', 'date', 'week_id', 'is_rec']]
-    models = df_is_rec['model_id'].unique()
-    dates = pd.date_range(start=week_id_to_date(cutoff), periods=prediction_length, freq='W')
-    m, d = pd.core.reshape.util.cartesian_product([models, dates])
-    df_is_rec_future = pd.DataFrame({"model_id": m, "date": d})
-    df_is_rec_future['week_id'] = date_to_week_id(df_is_rec_future['date'])
+    df_is_rec_future = pd.DataFrame({'model_id': m, 'week_id': w})
     df_is_rec_future['is_rec'] = 0
-    df_is_rec = df_is_rec.append(df_is_rec_future)
-    df_is_rec = df_is_rec[['model_id', 'week_id', 'is_rec']]
+    
+    df_is_rec = df[['model_id', 'week_id', 'is_rec']].append(df_is_rec_future)
 
     return df_is_rec
 
@@ -290,6 +262,15 @@ def features_forward_fill(df, cutoff, projection_length):
 
 def apply_first_lockdown_patch(df_sales, df_sales_imputed):
     """
+    Replaces the lockdown weeks of `df_sales` with the imputed ones of `df_sales_imputed`.
+    
+    Args:
+        df_sales (pd.DataFrame): The initial sales dataframe
+        df_sales_imputed (pd.DataFrame): The imputed sales dataframe
+        
+    Returns:
+        pd.DataFrame with the same structure as `df_sales`, whose lockdown weeks have been 
+        replaced by the ones in `df_sales_imputed`
     """
     df_sales = pd.merge(df_sales, df_sales_imputed, how='left')
     df_sales['sales_quantity'] = np.where(df_sales['sales_quantity_imputed'].notnull(),
