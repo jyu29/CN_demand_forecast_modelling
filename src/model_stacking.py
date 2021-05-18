7d70c0a2dbae42a742fa6988d4a15d3d8120f763
Ben Bouillet <benjybouillet@gmail.com>
2021-05-18 16:32:50 +0200

    PEP8 changes

--- src/model_stacking.py
+++ src/model_stacking.py
@@ -14,22 +14,22 @@ def calculate_deepar_arima_stacking(df_jobs, smooth_stacking_range=(8, 16)):
     """
     """
     assert all(a in df_jobs['algorithm'].unique() for a in ['deepar', 'arima']), \
-    "Deepar & arima must be included in the algorithms launched in the previous steps."
+        "Deepar & arima must be included in the algorithms launched in the previous steps."
 
     assert (all(isinstance(v, int) for v in smooth_stacking_range)) & \
            (smooth_stacking_range[0] > 1) & \
            (smooth_stacking_range[1] > smooth_stacking_range[0]), \
-    "The stacking range should be a tuple of 2 integer respecting (a, b): 1 < a < b."
+           "The stacking range should be a tuple of 2 integer respecting (a, b): 1 < a < b."
 
     list_cutoff = df_jobs['cutoff'].unique()
     stacking_start = smooth_stacking_range[0]
     stacking_stop = smooth_stacking_range[1]
     nb_stacking_weeks = stacking_stop - stacking_start
-    
+
     for cutoff in list_cutoff:
-        
+
         logger.info(f"Calculate deepar_arima for cutoff {cutoff}...")
-        
+
         # Set needed path
         df_jobs_cutoff = df_jobs[df_jobs['cutoff'] == cutoff].copy()
 
@@ -39,11 +39,11 @@ def calculate_deepar_arima_stacking(df_jobs, smooth_stacking_range=(8, 16)):
         arima_input = df_jobs_cutoff.loc[df_jobs['algorithm'] == 'arima', 'predict_path'].values[0]
         arima_output = arima_input.replace('input', 'output') + '.out'
 
-        deepar_arima_output = arima_output.replace('arima', 'deepar-arima') 
+        deepar_arima_output = arima_output.replace('arima', 'deepar-arima')
 
         # Load & format deepar forecast
-        logger.debug(f"Load & format Deepar forecasts...")
-        
+        logger.debug("Load & format Deepar forecasts...")
+
         deepar_model_id = read_jsonline_s3(*from_uri(deepar_input))['model_id'].values
         deepar = read_jsonline_s3(*from_uri(deepar_output)).set_index(deepar_model_id)
         deepar = pd.concat([deepar, deepar['quantiles'].apply(pd.Series)], axis=1)
@@ -54,28 +54,28 @@ def calculate_deepar_arima_stacking(df_jobs, smooth_stacking_range=(8, 16)):
         deepar = deepar.reset_index().rename(columns={'index': 'model_id'})
         deepar = deepar.apply(pd.Series.explode)
         deepar = deepar.astype(float).clip(lower=0).round().astype(int)
-        
+
         deepar['forecast_step'] = list(range(1, horizon + 1)) * deepar['model_id'].nunique()
         deepar.rename(columns={'0.5': 'forecast_deepar'}, inplace=True)
         deepar = deepar[['model_id', 'forecast_step', 'forecast_deepar']]
-        
+
         assert deepar['forecast_step'].max() >= stacking_stop, \
-        f"Deepar's forecast horizon is too short to apply stacking. Minimum horizon required: {stacking_stop}"
-        
+            f"Deepar's forecast horizon is too short to apply stacking. Minimum horizon required: {stacking_stop}"
+
         # Load & format arima forecast
-        logger.debug(f"Load & format Arima forecasts...")
-        
+        logger.debug("Load & format Arima forecasts...")
+
         arima = read_multipart_parquet_s3(*from_uri(arima_output))
         arima.rename(columns={'forecast': 'forecast_arima'}, inplace=True)
-        
+
         assert arima['forecast_step'].max() >= stacking_stop, \
-        f"Arima's forecast horizon is too short to apply stacking. Minimum horizon required: {stacking_stop}"
-        
+            f"Arima's forecast horizon is too short to apply stacking. Minimum horizon required: {stacking_stop}"
+
         # Calculate deepar_arima forecast with a smooth stacking
         deepar_arima = arima.merge(deepar, how='left')
-        
+
         deepar_arima['smooth_weight'] = (deepar_arima['forecast_step'] - nb_stacking_weeks) / nb_stacking_weeks
-        
+
         deepar_arima['forecast'] = np.where(
             deepar_arima['forecast_step'] <= stacking_start,
             deepar_arima['forecast_deepar'],
@@ -86,8 +86,8 @@ def calculate_deepar_arima_stacking(df_jobs, smooth_stacking_range=(8, 16)):
                 deepar_arima['forecast_arima']
             )
         ).round().astype(int)
-        
+
         # Format deepar_arima
         deepar_arima = deepar_arima[['model_id', 'forecast_step', 'forecast']]
-        
+
         write_df_to_parquet_on_s3(deepar_arima, *from_uri(deepar_arima_output), verbose=True)
