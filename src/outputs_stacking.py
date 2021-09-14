9802337f75ce11a9484d5fa089578698483765c1
Antoine-Schwartz <antoine.schwartz@decathlon.com>
2021-09-14 15:35:52 +0000

    fix, clean & docstring outputs stacking functions

--- src/outputs_stacking.py
+++ src/outputs_stacking.py
@@ -10,73 +10,150 @@ logging.basicConfig()
 logger.setLevel(logging.INFO)
 
 
-def read_format_stacking_data(df_jobs_cutoff):
 
+def read_format_deepar_forecast(input_path, output_path):
+    """Read and format Sagemaker Deepar jsonline outputs
+
+    Read the input jsonline file to bring back the model ids (not available in the output file), and
+    format outputs to a readable Dataframe format, including only the point estimate 'mean' forecasts.
+    
+    Args:
+        input_path (str): String path to the deepar jsonline input file
+        output_path (str): String path to the deepar jsonline output file
+
+    Returns:
+        forecast (pd.DataFrame): Formatted deepar forecast dataframe
+    """
+    logger.debug("Read & format deepar forecast...")
+    
+    model_ids = read_jsonline_s3(*from_uri(input_path))['model_id'].values
+    forecast = read_jsonline_s3(*from_uri(output_path)).set_index(model_ids)
+    forecast = pd.concat([forecast, forecast['quantiles'].apply(pd.Series)], axis=1)
+    forecast.drop(columns=['quantiles'], inplace=True)
+
+    horizon = len(forecast.iloc[0]['mean'])
+
+    forecast = forecast.reset_index().rename(columns={'index': 'model_id'})
+    forecast = forecast.apply(pd.Series.explode)
+    forecast = forecast.astype(float).clip(lower=0).round().astype(int)
+
+    forecast['forecast_step'] = list(range(1, horizon + 1)) * forecast['model_id'].nunique()
+    forecast.rename(columns={'0.5': 'forecast'}, inplace=True)
+    forecast = forecast[['model_id', 'forecast_step', 'forecast']]
+    
+    return forecast
+
+
+def read_format_forecast(df_jobs_cutoff, short_term_algorithm, long_term_algorithm):
+    """Read and format short and long term algorithm's forecasts
+
+    Args:
+        df_jobs_cutoff (pd.DataFrame): Run jobs information for a unique cutoff
+        short_term_algorithm (str): The short-term forecast algorithm name
+        long_term_algorithm (str): The long-term forecast algorithm name
+
+    Returns:
+        st_forecast (pd.DataFrame): Formatted short-term forecast dataframe
+        lt_forecast (pd.DataFrame): Formatted long-term forecast dataframe
+        stacking_output_path (str): The final stacked outputs path, calculated from the long-term forecast output path
+    """
     assert df_jobs_cutoff['cutoff'].nunique() == 1, '`df_jobs_cutoff` must contain only one cutoff'
 
-    deepar_input = df_jobs_cutoff[df_jobs_cutoff['algorithm'] == 'deepar']['predict_path'].values[0]
-    deepar_output = deepar_input.replace('input', 'output') + '.out'
+    # Set/calculate usefull paths
+    st_input_path = df_jobs_cutoff.loc[df_jobs_cutoff['algorithm'] == short_term_algorithm, 'predict_path'].values[0]
+    st_output_path = st_input_path.replace('input', 'output') + '.out'
 
-    hw_input = df_jobs_cutoff.loc[df_jobs_cutoff['algorithm'] == 'hw']['predict_path'].values[0]
-    hw_output = hw_input.replace('input', 'output') + '.out'
+    lt_input_path = df_jobs_cutoff.loc[df_jobs_cutoff['algorithm'] == long_term_algorithm, 'predict_path'].values[0]
+    lt_output_path = lt_input_path.replace('input', 'output') + '.out'
 
-    deepar_hw_output = hw_output.replace('hw', 'deepar-hw')
+    stacking_output_path = lt_output_path \
+        .replace(long_term_algorithm, f'{short_term_algorithm}-{long_term_algorithm}') \
+        .replace('json', 'parquet')
 
-    # Load & format deepar forecast
-    logger.debug("Load & format Deepar forecasts...")
+    # Read & format short term forecast
+    logger.debug("Read & format short term forecast...")
 
-    deepar_model_id = read_jsonline_s3(*from_uri(deepar_input))['model_id'].values
-    deepar = read_jsonline_s3(*from_uri(deepar_output)).set_index(deepar_model_id)
-    deepar = pd.concat([deepar, deepar['quantiles'].apply(pd.Series)], axis=1)
-    deepar.drop(columns=['quantiles'], inplace=True)
+    if short_term_algorithm == 'deepar':
+        st_forecast = read_format_deepar_forecast(st_input_path, st_output_path)
+    else:
+        st_forecast = read_multipart_parquet_s3(*from_uri(st_output_path))
+    st_forecast.rename(columns={'forecast': 'st_forecast'}, inplace=True)
+        
+    # Read & format long term forecast
+    logger.debug("Read & format short term forecast...")
 
-    horizon = len(deepar.iloc[0]['mean'])
+    if long_term_algorithm == 'deepar':
+        lt_forecast = read_format_deepar_forecast(lt_input_path, lt_output_path)
+    else:
+        lt_forecast = read_multipart_parquet_s3(*from_uri(lt_output_path))
+    lt_forecast.rename(columns={'forecast': 'lt_forecast'}, inplace=True)
 
-    deepar = deepar.reset_index().rename(columns={'index': 'model_id'})
-    deepar = deepar.apply(pd.Series.explode)
-    deepar = deepar.astype(float).clip(lower=0).round().astype(int)
-
-    deepar['forecast_step'] = list(range(1, horizon + 1)) * deepar['model_id'].nunique()
-    deepar.rename(columns={'0.5': 'forecast_deepar'}, inplace=True)
-    deepar = deepar[['model_id', 'forecast_step', 'forecast_deepar']]
-
-    # Load & format hw forecast
-    logger.debug("Load & format hw forecasts...")
-
-    hw = read_multipart_parquet_s3(*from_uri(hw_output))
-    hw.rename(columns={'forecast': 'forecast_hw'}, inplace=True)
-
-    return deepar, hw, deepar_hw_output
+    return st_forecast, lt_forecast, stacking_output_path
 
 
-def compute_stacking(deepar, hw, stacking_start, stacking_stop, nb_stacking_weeks):
-    # Calculate deepar_hw forecast with a smooth stacking
-    deepar_hw = hw.merge(deepar, how='left')
+def compute_stacking(st_forecast, lt_forecast, stacking_start, stacking_stop):
+    """Calculate a smooth linear stacking between short & long term forecast during a specified period
 
-    deepar_hw['smooth_weight'] = (deepar_hw['forecast_step'] - stacking_start) / nb_stacking_weeks
+    Args:
+        st_forecast (pd.DataFrame): Formatted short-term forecast dataframe
+        lt_forecast (pd.DataFrame): Formatted long-term forecast dataframe
+        stacking_start (int): First stacking forecast step 
+        stacking_start (int): Last stacking forecast step
 
-    deepar_hw['forecast'] = np.where(
-        deepar_hw['forecast_step'] <= stacking_start,
-        deepar_hw['forecast_deepar'],
+    Returns:
+        stacked_forecast (pd.DataFrame): The stacked forecast dataframe
+    """
+    assert st_forecast['forecast_step'].max() >= stacking_stop, \
+        f"Short-term algorithm's forecast horizon is too short to apply stacking. Minimum horizon required: {stacking_stop}"
+
+    assert lt_forecast['forecast_step'].max() >= stacking_stop, \
+        f"Long-term algorithm's forecast horizon is too short to apply stacking. Minimum horizon required: {stacking_stop}"
+
+    # Calculate stacking
+    nb_stacking_weeks = stacking_stop - stacking_start
+    
+    stacked_forecast = pd.merge(lt_forecast, st_forecast, how='left')
+
+    stacked_forecast['smooth_weight'] = (stacked_forecast['forecast_step'] - stacking_start) / nb_stacking_weeks
+
+    stacked_forecast['forecast'] = np.where(
+        stacked_forecast['forecast_step'] <= stacking_start,
+        stacked_forecast['st_forecast'],
         np.where(
-            deepar_hw['forecast_step'] <= stacking_stop,
-            deepar_hw['forecast_deepar'] * (1 - deepar_hw['smooth_weight']) + \
-            deepar_hw['forecast_hw'] * deepar_hw['smooth_weight'],
-            deepar_hw['forecast_hw']
+            stacked_forecast['forecast_step'] <= stacking_stop,
+            stacked_forecast['st_forecast'] * (1 - stacked_forecast['smooth_weight']) + \
+            stacked_forecast['lt_forecast'] * stacked_forecast['smooth_weight'],
+            stacked_forecast['lt_forecast']
         )
     ).round().astype(int)
 
-    # Format deepar_hw
-    deepar_hw = deepar_hw[['model_id', 'forecast_step', 'forecast']]
+    # Format stacked forecast
+    stacked_forecast = stacked_forecast[['model_id', 'forecast_step', 'forecast']]
 
-    return deepar_hw
+    return stacked_forecast
 
 
-def calculate_outputs_stacking(df_jobs, smooth_stacking_range=(10, 16)):
+def calculate_outputs_stacking(df_jobs,
+                               short_term_algorithm='deepar',
+                               long_term_algorithm='hw',
+                               smooth_stacking_range=(10, 16)):
+    """Calculate linear smooth stacking between forecasts of 2 algorithms during a specific period, and write it on S3
+
+    Args:
+        df_jobs (pd.DataFrame): Previous run jobs information
+        short_term_algorithm (str): The short-term forecast algorithm name
+        long_term_algorithm (str): The long-term forecast algorithm name
+        smooth_stacking_range (tuple): Tuple defining the period, i.e. the first and last forecast step (exluded), 
+            during which stacking is applied
+            Ex: (10, 16) means 100% of short-term algorithm's forecast until forecast step 10 (included), 
+                then an evolving mix of short and long-term algorithm's forecast between forecast step 11 and 15,
+                and finally the long term algorithm's forecast from forecast step 16
+
     """
-    """
-    assert all(a in df_jobs['algorithm'].unique() for a in ['deepar', 'hw']), \
-        "Deepar & hw must be included in the algorithms launched in the previous steps."
+    assert df_jobs['algorithm'].nunique() == 2, "df_jobs should contains exactly 2 differents algorithms for stacking."
+    
+    assert all(a in df_jobs['algorithm'].unique() for a in [short_term_algorithm, long_term_algorithm]), \
+        f"{short_term_algorithm} & {long_term_algorithm} must the 2 algorithms launched in the previous steps."
 
     assert (all(isinstance(v, int) for v in smooth_stacking_range)) & \
            (smooth_stacking_range[0] > 1) & \
@@ -86,23 +163,18 @@ def calculate_outputs_stacking(df_jobs, smooth_stacking_range=(10, 16)):
     list_cutoff = df_jobs['cutoff'].unique()
     stacking_start = smooth_stacking_range[0]
     stacking_stop = smooth_stacking_range[1]
-    nb_stacking_weeks = stacking_stop - stacking_start
 
     for cutoff in list_cutoff:
 
-        logger.info(f"Calculate deepar_hw for cutoff {cutoff}...")
+        logger.info(f"Calculate {short_term_algorithm}-{long_term_algorithm} for cutoff {cutoff}...")
 
-        # Set needed path
         df_jobs_cutoff = df_jobs[df_jobs['cutoff'] == cutoff].copy()
 
-        deepar, hw, deepar_hw_output = read_format_stacking_data(df_jobs_cutoff)
+        st_forecast, lt_forecast, stacking_output_path = read_format_forecast(df_jobs_cutoff,
+                                                                              short_term_algorithm,
+                                                                              long_term_algorithm)
 
-        assert deepar['forecast_step'].max() >= stacking_stop, \
-            f"Deepar's forecast horizon is too short to apply stacking. Minimum horizon required: {stacking_stop}"
+        stacked_forecast = compute_stacking(st_forecast, lt_forecast, stacking_start, stacking_stop)
 
-        assert hw['forecast_step'].max() >= stacking_stop, \
-            f"hw's forecast horizon is too short to apply stacking. Minimum horizon required: {stacking_stop}"
-
-        deepar_hw = compute_stacking(deepar, hw, stacking_start, stacking_stop, nb_stacking_weeks)
-
-        write_df_to_parquet_on_s3(deepar_hw, *from_uri(deepar_hw_output), verbose=True)
+        #write_df_to_parquet_on_s3(stacked_forecast, *from_uri(stacking_output_path), verbose=True)
+        return stacked_forecast
